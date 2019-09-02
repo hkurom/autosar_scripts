8e157a2205ef8016fd5dddcaa59d9848ebff4be4
kurosawa.hiroomi <kurosawa.hiroomi@canon-its.co.jp>
2019-09-02 19:09:38 +0900

    bugfix

--- memmap_define_checker/memmap_define_checker.rb
+++ memmap_define_checker/memmap_define_checker.rb
@@ -1,17 +1,21 @@
 require "find"
 require "pp"
+require "optparse"
 
 if ARGV.empty? || ARGV.count() < 1
   puts "Usage: " + File.basename($0) + ' [path]...'
   exit 1
 end
 
+exclude_dirs = ARGV.getopts("exclude:")
+p exclude_dirs if $DEBUG
+
 memmap_defines = []
 other_defines  = []
 
 ARGV.each{|path|
   Find.find(path) {|file|
-    next if !file.match?(/\.[ch]$/)
+    next if !file.match(/\.[ch]$/)
     defines = []
     if File.basename(file).include?("MemMap.h")
       defines = memmap_defines
@@ -33,3 +37,5 @@ memmap_defines.uniq!
 other_defines.each {|define|
   pp define unless memmap_defines.include?(define)
 }
+
+pp memmap_defines if $DEBUG
